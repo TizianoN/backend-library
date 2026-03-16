0c878a4bc84c452ec155a1369faaaa9c56ce67f5
TizianoN <hello@tizianonicolai.com>
2026-03-16 11:52:46 +0100

    add CRUD routes

--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const bookController = require("../controllers/bookController");
 
 router.get("/", bookController.index);
+router.get("/:id", bookController.show);
+router.post("/", bookController.store);
+router.put("/:id", bookController.update);
+router.patch("/:id", bookController.modify);
+router.delete("/:id", bookController.destroy);
 
 module.exports = router;
