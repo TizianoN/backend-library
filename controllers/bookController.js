8d36e345ade5dd38b3ed1439cb7e5034982a49ad
TizianoN <hello@tizianonicolai.com>
2026-03-20 12:46:50 +0100

    add store + review

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,13 +4,17 @@ const { handleFailedQuery, handleResourceNotFound } = require("../utils/database
 function index(req, res) {
   const booksSQL = `
     SELECT 
-        books.*,
-        AVG(reviews.vote) avg_vote
+      books.id,
+      books.title,
+      books.author,
+      books.abstract,
+      books.image,
+      AVG(reviews.vote) average_vote
 
     FROM 
-        library.books
+        books
 
-    INNER JOIN reviews
+    LEFT JOIN reviews
     ON books.id = reviews.book_id
 
     GROUP BY books.id`;
@@ -21,7 +25,7 @@ function index(req, res) {
     const books = bookResult.map((book) => {
       return {
         ...book,
-        avg_vote: parseInt(book.avg_vote),
+        average_vote: parseInt(book.average_vote),
         image: buildBookImgPath(book.image),
       };
     });
@@ -33,13 +37,22 @@ function index(req, res) {
 function show(req, res) {
   const { id } = req.params;
 
-  const booksSQL = `SELECT * FROM books WHERE id = ?`;
+  const booksSQL = `
+    SELECT books.*
+    FROM  library.books
+    WHERE id = ?`;
+
   connection.query(booksSQL, [id], (err, bookResult) => {
     if (err) return handleFailedQuery(err, res);
     const [book] = bookResult;
     if (!book) return handleResourceNotFound(res);
 
-    const reviewSQL = `SELECT * FROM reviews WHERE book_id = ?`;
+    const reviewSQL = `
+      SELECT * 
+      FROM reviews 
+      WHERE book_id = ? 
+      ORDER BY created_at DESC
+    `;
     connection.query(reviewSQL, [id], (err, reviewsResult) => {
       if (err) return handleFailedQuery(err, res);
       book.reviews = reviewsResult;
@@ -50,9 +63,43 @@ function show(req, res) {
   });
 }
 
-// function store(req, res) {
-//   res.json({ message: "WIP" });
-// }
+function store(req, res) {
+  const { filename } = req.file;
+  const { title, author, abstract } = req.body;
+
+  const storeBookSQL = `INSERT INTO library.books 
+  (title, author, abstract, image) 
+  VALUES (?, ?, ?, ?);`;
+
+  connection.query(storeBookSQL, [title, author, abstract, filename], (err, result) => {
+    const { insertId } = result;
+    res.status(201).json({ insertId });
+  });
+}
+
+function storeReview(req, res) {
+  const { id } = req.params;
+  const { name, vote, text } = req.body;
+
+  console.log(id, name, vote, text);
+
+  const storeReviewSQL = `
+  INSERT INTO library.reviews
+  (book_id, name, vote, text) VALUES
+  (?, ?, ?, ?);
+  `;
+
+  connection.query(storeReviewSQL, [id, name, vote, text], (err, result) => {
+    const { insertId } = result;
+    res.status(201).json({ insertId });
+
+    // const showReviewSQL = `SELECT * from reviews WHERE id = ?`;
+    // connection.query(showReviewSQL, [result.insertId], (err, result) => {
+    //   const [review] = result;
+    //   res.json(review);
+    // });
+  });
+}
 
 // function update(req, res) {
 //   res.json({ message: "WIP" });
@@ -69,7 +116,8 @@ function show(req, res) {
 module.exports = {
   index,
   show,
-  //   store,
+  store,
+  storeReview,
   //   update,
   //   modify,
   //   destroy,
--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/bookController");
+const upload = require("../middlewares/multer");
 
 router.get("/", bookController.index);
 router.get("/:id", bookController.show);
-// router.post("/", bookController.store);
+router.post("/", upload.single("image"), bookController.store);
+router.post("/:id/review", bookController.storeReview);
 // router.put("/:id", bookController.update);
 // router.patch("/:id", bookController.modify);
 // router.delete("/:id", bookController.destroy);
